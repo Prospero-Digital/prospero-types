0e57823347905e9dd54963fbc6b7284900e639b2
Al Pagan <generamics@gmail.com>
2024-03-18 15:35:59 +0000

    Made tags optional

--- src/bundles.ts
+++ src/bundles.ts
@@ -31,7 +31,7 @@ export type Bundle = {
   producerName?: string;
   producerAvatar?: MediaImage;
   smartscriptIds: string[];
-  tags: string[];
+  tags?: string[];
   themeId?: string;
   thumbnail?: Media;
   title: string;
