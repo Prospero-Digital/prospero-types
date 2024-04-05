f202ad3d8ac106bcd33412d09eeece76fa1ad098
Al Pagan <generamics@gmail.com>
2024-04-05 14:02:19 +0100

    First part of typing variables for assignment progress tables

--- src/render/variables.ts
+++ src/render/variables.ts
@@ -1,3 +1,32 @@
+import {
+  MediaImage,
+  MediaVideo,
+  MediaAudio,
+  MediaGoogleMap,
+  MediaGoogleMapDirections,
+} from '../media';
+
+export type VariableType =
+  | 'text'
+  | 'number'
+  | 'bool'
+  | 'image'
+  | 'video'
+  | 'audio'
+  | 'googleMaps'
+  | 'googleMapsDirections'
+  | 'genres';
+
+export type VariableValue =
+  | string
+  | number
+  | boolean
+  | MediaImage
+  | MediaVideo
+  | MediaAudio
+  | MediaGoogleMap
+  | MediaGoogleMapDirections;
+
 export type Variable = {
   name: string;
   level: 'global' | 'actor' | 'user';
